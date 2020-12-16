636a04368f16a724fb8cc8c8eac1e00f47effcbc
Cicero ferreira <ciceronascimentu@gmail.com>
2020-12-16 15:12:22 -0300

    Adicionar arquivos de projeto.

---
+++ AccountController/Entities/Exception/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AccountController.Entities.DomainException
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ AccountController/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using AccountController.Entities.DomainException;
+
+namespace AccountController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                Account account = new Account(number, name, initialBalance, withdrawLimit);
+
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine();
+                Console.WriteLine("New balance: " + account.Balance.ToString(), "F2", CultureInfo.InvariantCulture);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
